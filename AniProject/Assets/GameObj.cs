dd665d79b87c71396c3b1b835665ddad27262c4d
Unknown <jakedafro@gmail.com>
2022-06-23 10:59:19 -0600

    Adding game objects

---
+++ AniProject/Assets/GameObj.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObj : MonoBehaviour
+{
+  // Use this for initialization.
+  void start ()
+  {
+      Debug.Log("I am alive!");
+  }
+         
+}
