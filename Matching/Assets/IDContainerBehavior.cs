1c503db722a751ebc280f9781eab3d9b42e58660
Unknown <jakedafro@gmail.com>
2022-07-30 16:06:23 -0600

    Fist part of Matching Game Made

---
+++ Matching/Assets/IDContainerBehavior.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class IDContainerBehavior : MonoBehaviour
+{
+    public ID idObj;
+    public UnityEvent startEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       startEvent.Invoke();
+    }
+
+    
+}
