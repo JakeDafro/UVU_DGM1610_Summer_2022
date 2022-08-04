d799af22452fae1d99660fa64aed8f58a7eb364c
Unknown <jakedafro@gmail.com>
2022-08-04 16:25:50 -0600

    7bLab

---
+++ Coroutine/Assets/Scripts/SpriteBehavior.cs
@@ -0,0 +1,24 @@
+
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class SpriteBehavior : MonoBehaviour
+{
+    private SpriteRenderer rendererObj;
+
+    private void Awake()
+    {
+        rendererObj = GetComponent<SpriteRenderer>();
+    }
+
+    public void ChangeRendererColor(ColorID obj)
+    {
+        rendererObj.color = obj.value;
+    }
+
+    public void ChangeRenderColor(ColorIDDataList obj)
+    {
+        rendererObj.color = obj.currentColor.value;
+    }
+}
